89bd01c0db97055b6116bbbad8b7a6b1cd6d2392
MBGNSVKrishna <mbgnsvk99@outlook.com>
2021-07-30 07:37:17 +0530

    Add project files.

---
+++ log4netdemo/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using log4netdemo.CustomFilter;
+using log4netdemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace log4netdemo.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(HomeController));
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [CatchError]
+        public IActionResult Exception(int? id)
+        {
+            try
+            {
+                if (id == null)
+                    throw new Exception("Error Id cannot be null");
+                else
+                    return View((object)$"The value is {id}");
+            }
+            catch (Exception e)
+            {
+                _log4net.Error(e.Message);
+                return View();
+            }
+         
+
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            // _log4net.Info("Hello logging world log4net!");
+            _log4net.Warn("error occured");
+            return View();
+
+        }
+   /*     public IActionResult Index1()
+        {
+            _log4net.Info("Hello It's me!");
+            return View();
+        }*/
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ log4netdemo/CustomFilter/CatchError.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace log4netdemo.CustomFilter
+{
+    public class CatchError : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ViewResult()
+            {
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+                {
+                    Model = context.Exception.Message
+                }
+            };
+        }
+    }
+}
